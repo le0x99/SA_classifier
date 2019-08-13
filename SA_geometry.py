28bd89cde6fc4be7ae243c35e9f1f72e901d8251
le0x99 <36996220+le0x99@users.noreply.github.com>
2019-08-13 19:33:13 +0200

    bug fixed, where target polygon objects holds only 1 element

--- SA_geometry.py
+++ SA_geometry.py
@@ -201,10 +201,13 @@ def gsv_point(facade, target, radius=15):
     data = response.json()
     if data["status"] == "OK":
         gsv_point = Point((data["location"]["lat"], data["location"]["lng"]))
-        for t in target:
-            if facade.touches(t.boundary) or  facade.within(t.boundary):
-                if gsv_point.within(t) or gsv_point.touches(t):
-                    return gsv_point
+	try:
+		for t in target:
+		    if facade.touches(t.boundary) or  facade.within(t.boundary):
+			if gsv_point.within(t) or gsv_point.touches(t):
+			    return gsv_point
+	except:
+		return gsv_point
     else:        
         return 
     
