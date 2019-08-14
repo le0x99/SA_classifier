62f4c6770f63e0a5c68643508a238ca15a5487e4
le0x99 <36996220+le0x99@users.noreply.github.com>
2019-08-14 18:11:13 +0200

    Update SA_geometry.py

--- SA_geometry.py
+++ SA_geometry.py
@@ -6,8 +6,10 @@ import warnings
 from math import radians, sin, asin, cos, tan, atan, sqrt
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, LineString, Polygon, MultiPoint, MultiPolygon, MultiLineString
+from shapely.wkt impot loads as to_geo
 import shapely
 from UliEngineering.Math.Coordinates import BoundingBox
+
 
 if 'ipykernel' in sys.modules:
 	plt.rcParams["figure.dpi"] = 86
