ee9f49013eb7723155d78874954a32d60d8a1fbb
le0x99 <36996220+le0x99@users.noreply.github.com>
2019-08-19 15:25:21 +0200

    Update build_network.py

--- build_network.py
+++ build_network.py
@@ -1,19 +1,13 @@
-## Script to build the full model
-
 from keras.models import Sequential, load_model, Model
 from keras.layers import Dropout, Flatten, Dense
 from keras import applications, optimizers
 
 
-config_dir = "/Users/Leonard/Desktop/NN_imp/SA_classifier/config/"
-train_data_dir = "/Users/Leonard/Desktop/NN_imp/SA_classifier/data/training"
-validation_data_dir = "/Users/Leonard/Desktop/NN_imp/SA_classifier/data/validation"
-top_model_path = config_dir+"model_bn_weighted.h5"
-top_model_weights_path = config_dir+"bottleneck_weights.h5"
 
-img_width, img_height = 150, 150
-nb_train_samples, nb_validation_samples = 400, 40
-epochs, batch_size = 50, 16
+top_model_path = "model_bn_weighted.h5"
+top_model_weights_path = "bottleneck_weights.h5"
+
+img_width, img_height = 224, 224
 
 
 # Instantiate the VGG16 network
@@ -30,4 +24,4 @@ top_model = load_model(top_model_path)
 full_model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
 
 # save the full model
-full_model.save(config_dir+"SA_classifier.h5")
+full_model.save("SA_classifier.h5")
