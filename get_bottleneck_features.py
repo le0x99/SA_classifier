46baf616021e03009fb48f60a6abf0e2be77d7d5
le0x99 <36996220+le0x99@users.noreply.github.com>
2019-08-19 15:25:47 +0200

    Update get_bottleneck_features.py

--- get_bottleneck_features.py
+++ get_bottleneck_features.py
@@ -15,12 +15,11 @@ from keras import applications
 #### Pre-Settings
 
 
-train_data_dir = "/Users/Leonard/Desktop/NN_imp/SA_classifier/data/training"
-validation_data_dir = "/Users/Leonard/Desktop/NN_imp/SA_classifier/data/validation"
-config_dir = "/Users/Leonard/Desktop/NN_imp/SA_classifier/config/"
-img_width, img_height = 150, 150
-nb_train_samples, nb_validation_samples = 400, 40
-epochs, batch_size = 50, 16
+train_data_dir = "data\\training"
+validation_data_dir = "data\\validation"
+img_width, img_height = 224, 224
+nb_train_samples, nb_validation_samples = 520, 100
+epochs, batch_size = 200, 16
 
 #### Get the bottleneck features
 
@@ -42,7 +41,7 @@ predict_size_train = int(ceil(nb_train_samples / batch_size))
 # predict_generator returns the output of a model, given
 # a generator that yields batches of data
 bottleneck_features_train = model.predict_generator(generator, predict_size_train)
-with open(config_dir+'bottleneck_features_train.npy', 'wb') as features_train_file:
+with open('bottleneck_features_train.npy', 'wb') as features_train_file:
     np.save(features_train_file, bottleneck_features_train)
 
 #generate validation data from the gsv images
@@ -54,20 +53,20 @@ generator = datagen.flow_from_directory(
     shuffle=False)
 predict_size_validation = int(ceil(nb_validation_samples / batch_size))
 bottleneck_features_validation = model.predict_generator(generator, predict_size_validation)
-with open(config_dir+'bottleneck_features_validation.npy', "wb") as features_validation_file:
+with open('bottleneck_features_validation.npy', "wb") as features_validation_file:
     np.save(features_validation_file, bottleneck_features_validation)
 
 
 ## Train the small top model 
     
     
-with open(config_dir+'bottleneck_features_train.npy', 'rb') as features_train_file:
+with open('bottleneck_features_train.npy', 'rb') as features_train_file:
     train_data = np.load(features_train_file)
     # we now have to label the data again 80:0, 80:1
 train_labels = np.array(
     [0] * (nb_train_samples // 2) + [1] * (nb_train_samples // 2))
 
-with open(config_dir+'bottleneck_features_validation.npy', 'rb') as features_validation_file:
+with open('bottleneck_features_validation.npy', 'rb') as features_validation_file:
     validation_data = np.load(features_validation_file)
     
 validation_labels = np.array(
@@ -76,6 +75,10 @@ validation_labels = np.array(
 # Instantiate the top model, which can be seen as the classification model 
 model = Sequential()#sequential because we add layer by layer
 model.add(Flatten(input_shape=train_data.shape[1:]))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
@@ -87,8 +90,21 @@ model.fit(train_data, train_labels,
           batch_size=batch_size,
           validation_data=(validation_data, validation_labels))
 # save its weights
-model.save_weights(config_dir+"bottleneck_weights")
+model.save_weights("bottleneck_weights")
 # save the whole model
-model.save(config_dir+"model_bn_weighted.h5")
+model.save("model_bn_weighted.h5")
+import pandas as pd
+h = pd.DataFrame(model.history.history)
+h["Epoch"] = list(range(1, 201))
+h["train_acc"] = h["acc"]
+h["train_loss"] = h["loss"]
+
+plot = h.plot(y=["train_acc", "val_acc"], x="Epoch")
+fig = plot.get_figure()
+fig.savefig("network_accuracy.png")
+
+plot = h.plot(y=["train_loss", "val_loss"], x="Epoch")
+fig = plot.get_figure()
+fig.savefig("network_loss.png")
 
     
